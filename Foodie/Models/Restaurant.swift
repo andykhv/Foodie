7f74774bf9cdd72d5795919f55793d8a2d0bc725
Andy Khov <khovandyak@gmail.com>
2019-04-29 20:27:11 -0700

    init models for restaurant query and json parsing

---
+++ Foodie/Models/Restaurant.swift
@@ -0,0 +1,42 @@
+//
+//  Restaurant.swift
+//  Foodie
+//
+//  Created by Andy Khov on 4/29/19.
+//  Copyright © 2019 Andy Khov. All rights reserved.
+//
+
+// Decodable structs for JSON parsing Yelp Fusion Business Endpoint
+struct Restaurant: Decodable {
+    let id: String
+    let name: String
+    let image_url: String
+    let url: String
+    let phone: String
+    let review_count: Int
+    let rating: Float
+    let location: Location
+    let price: String
+    let photos: [String]
+    let hours: [Hours]
+}
+
+struct Location: Decodable {
+    let address1: String
+    let address2: String
+    let city: String
+    let zip_code: String
+    let country: String
+    let state: String
+}
+
+struct Hours: Decodable {
+    let open: [Hour]
+    let is_open_now: Bool
+}
+
+struct Hour: Decodable {
+    let start: String
+    let end: String
+    let day: Int
+}
---
+++ Foodie/Models/RestaurantQuery.swift
@@ -0,0 +1,24 @@
+//
+//  RestaurantQuery.swift
+//  Foodie
+//
+//  Created by Andy Khov on 4/28/19.
+//  Copyright © 2019 Andy Khov. All rights reserved.
+//
+
+class RestaurantQuery {
+    // MARK: properties
+    var query: String
+    var location: String?
+    var latitude: Float?
+    var longitude: Float?
+    var budget: Int
+    var distance: Int
+    
+    // MARK: init
+    init() {
+        self.query = ""
+        self.budget = 1
+        self.distance = 1
+    }
+}
