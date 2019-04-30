e46047da02246ba637011e9270ba1efa1d976dab
Andy Khov <khovandyak@gmail.com>
2019-04-29 21:36:39 -0700

    add test for yelp fusion json to Restaurant

--- FoodieTests/FoodieTests.swift
+++ FoodieTests/FoodieTests.swift
@@ -30,5 +30,80 @@ class FoodieTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
+    func testJsonToRestaurant() {
+        let data = """
+        {
+          "id": "WavvLdfdP6g8aZTtbBQHTw",
+          "alias": "gary-danko-san-francisco",
+          "name": "Gary Danko",
+          "image_url": "https://s3-media2.fl.yelpcdn.com/bphoto/CPc91bGzKBe95aM5edjhhQ/o.jpg",
+          "is_claimed": true,
+          "is_closed": false,
+          "url": "https://www.yelp.com/biz/",
+          "phone": "+14157492060",
+          "review_count": 5296,
+          "rating": 4.5,
+          "location": {
+            "address1": "800 N Point St",
+            "address2": "",
+            "address3": "",
+            "city": "San Francisco",
+            "zip_code": "94109",
+            "country": "US",
+            "state": "CA",
+            "display_address": [
+              "800 N Point St",
+              "San Francisco, CA 94109"
+            ],
+            "cross_streets": ""
+          },
+          "coordinates": {
+            "latitude": 37.80587,
+            "longitude": -122.42058
+          },
+          "photos": [
+            "https://s3-media2.fl.yelpcdn.com/bphoto/CPc91bGzKBe95aM5edjhhQ/o.jpg",
+            "https://s3-media4.fl.yelpcdn.com/bphoto/FmXn6cYO1Mm03UNO5cbOqw/o.jpg",
+            "https://s3-media4.fl.yelpcdn.com/bphoto/HZVDyYaghwPl2kVbvHuHjA/o.jpg"
+          ],
+          "price": "$$$$",
+          "hours": [
+            {
+              "open": [
+                {
+                  "is_overnight": false,
+                  "start": "1730",
+                  "end": "2200",
+                  "day": 0
+                },
+                {
+                  "is_overnight": false,
+                  "start": "1730",
+                  "end": "2200",
+                  "day": 1
+                },
+                {
+                  "is_overnight": false,
+                  "start": "1730",
+                  "end": "2200",
+                  "day": 2
+                },
+              ],
+              "hours_type": "REGULAR",
+              "is_open_now": false
+            }
+          ],
+        }
+        """.data(using: .utf8)
+        if let unwrappedData = data {
+            do {
+                let myStruct = try JSONDecoder().decode(Restaurant.self, from: unwrappedData)
+                print(myStruct)
+            } catch {
+                print("error")
+            }
+        }
+    }
+    
 }
