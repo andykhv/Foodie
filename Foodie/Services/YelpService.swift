5b3bfd197e35470e230a0b9902caeeb8d504471e
Andy Khov <khovandyak@gmail.com>
2019-05-15 20:26:48 -0700

    implement searchBusinesses method

--- Foodie/Services/YelpService.swift
+++ Foodie/Services/YelpService.swift
@@ -8,16 +8,27 @@
 
 import Foundation
 import Alamofire
+import os.log
 
-func searchRestaurants(with query: RestaurantQuery) -> Void {
+let BUSINESS_SEARCH_ENDPOINT = "https://api.yelp.com/v3/businesses/search"
+
+func searchRestaurants(with query: RestaurantQuery, completion completionHander: @escaping (DataResponse<Any>) -> Void) -> Void {
+    guard let API_KEY = getApiKey() else {
+        os_log("Yelp API Key not found", log: OSLog.default, type: .error)
+        return
+    }
     let params = buildParams(with: query)
-    Alamofire.request("https://api.yelp.com/v3/businesses/search")
+    let headers: HTTPHeaders = [
+        "Authorization": "Bearer \(API_KEY)"
+    ]
+    Alamofire.request(BUSINESS_SEARCH_ENDPOINT, parameters: params, headers: headers).responseJSON(completionHandler: completionHander)
 }
 
 private func buildParams(with query: RestaurantQuery) -> Parameters {
     var params = Parameters()
     params["term"] = query.term
-    params["radius"] = query.radius
+    /* radius not used, because it significantly decreases the restaurant results */
+    //params["radius"] = query.radius
     params["price"] = String(query.price)
     params["categories"] = "food"
     
@@ -29,4 +40,17 @@ private func buildParams(with query: RestaurantQuery) -> Parameters {
     }
     
     return params
+}
+
+private func getApiKey() -> String? {
+    if  let path = Bundle.main.path(forResource: "Keys", ofType: "plist"),
+        let xml = FileManager.default.contents(atPath: path) {
+        let keys = try? PropertyListSerialization.propertyList(from: xml, options: .mutableContainersAndLeaves, format: nil) as? [String]
+        guard let key = keys?[0] else {
+            return nil
+        }
+        return key
+    }
+    
+    return nil
 }
